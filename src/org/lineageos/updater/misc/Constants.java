4ebcba62a65323ab1e779b38f7211227ba01be60
Jens Reidel <adrian@travitia.xyz>
2022-12-23 21:26:13 +0100

    updater: Rename ro.arrow.* props to ro.kenvyra.*
    Change-Id: Ie4fdaf51bc64b9b2dec19ecbd8ae8f5ddd5bb895
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -48,10 +48,10 @@ public final class Constants {
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
     public static final String PROP_BUILD_VERSION = "ro.modversion";
     //public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";
-    public static final String PROP_DEVICE = "ro.arrow.device";
+    public static final String PROP_DEVICE = "ro.kenvyra.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_RELEASE_TYPE = "ro.arrow.releasetype";
-    public static final String PROP_ZIP_TYPE = "ro.arrow.ziptype";
+    public static final String PROP_RELEASE_TYPE = "ro.kenvyra.releasetype";
+    public static final String PROP_ZIP_TYPE = "ro.kenvyra.ziptype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "arrow.updater.uri";
     public static final String PROP_BLOCK_UPDATER = "ro.block_updater";
